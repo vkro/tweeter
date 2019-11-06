c689a01aaca5af4cf0345e185127517b0460024a
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-06 00:02:54 +0000

    keypress increments (number) value of .counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-
   $("#text").keypress(function() {
-    console.log((".counter span").text);
+    let count = $(".counter").text();
+    count = Number(count);
+    count++;
+    $(".counter").text(count);
   });
-
-
 
 });
