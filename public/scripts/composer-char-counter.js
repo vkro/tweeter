7a68348bc2a542f77877bff582a38a7615fd41eb
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-06 01:30:19 +0000

    set counter id value to negative if count is greater than char limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
   $("#text").keyup(function() {
-    let counter = $(".counter").text();
+    let counter = $(this).parent().children("span");
     let count = ($(this).context.value).length;
-    $(".counter").text((140 - count));
+    if (140 < count) {
+      counter.attr("id", "negative");
+    } else {
+      counter.attr("id", "positive")
+    }
+    $(counter).text((140 - count));
   });
 
 });
