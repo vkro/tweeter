3950af121a0c2f024b78f34a55acb000c03c64da
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-06 23:23:03 +0000

    createTweetElement will create DOM structure for given tweet object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,49 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+// Recall that you can use jQuery to construct new elements using the $ function, 
+// like so: const $tweet = $("<article>").addClass("tweet");
+
+// $( ".inner" ).append( "<p>Test</p>" );
+
+
+/*
+ * Takes in tweet object 
+ * Returns tweet <article> containing HTML structure of tweet
+*/
+
+
+$(document).ready(function() {
+  
+  const createTweetElement = function (tweet) {
+    const $tweet = $("<article>").addClass("tweet");
+    const $header = $("<header>").addClass("tweet-header");
+    const $img = $("<img>").addClass("tweet-avatar");
+    const $name = $("<span>").addClass("tweet-name")
+    const $handle = $("<span>").addClass("tweet-handl");
+    const $body = $("<p>").addClass("tweet-body");
+  
+    const $theTweet = $($tweet).append($header, $img, $name);
+  
+    return $theTweet;
+  };
+
+  const test = createTweetElement(tweetData);
+  console.log(test);
+});
+
+
+
