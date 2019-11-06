9435aac7a40280d31535cb0395133267d366f4e5
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-06 20:26:22 +0000

    reduce redundant code with var for # characters left

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
   $("#text").keyup(function() {
     let counter = $(this).parent().children("span");
     let count = ($(this).context.value).length;
-    if (140 < count) {
+    let charsLeft = 140 - count;
+    if (charsLeft < 0) {
       counter.attr("id", "negative");
     } else {
       counter.attr("id", "positive")
     }
-    $(counter).text((140 - count));
+    $(counter).text(charsLeft);
   });
 
 });
