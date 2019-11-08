39db4297d865c2dc18ea37b53f3d875c6d8a81f3
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-08 22:09:44 +0000

    add focus to text box on tweet slider click

--- public/scripts/compose-tweet-slider.js
+++ public/scripts/compose-tweet-slider.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $(".write-new-tweet").click(function() {
     $(".validation-error").hide(0, "swing");
     $(".new-tweet").slideToggle("slow");
+    $("#textArea").focus();
   });
 });
 
