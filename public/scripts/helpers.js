243dfb7fdcf00e61552bf78410120e18196003c5
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-09 00:04:29 +0000

    moving time calculation helpers to be used as a script

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,32 +1,39 @@
 // Takes in number representing # of seconds
-// Converts into minutes, days, hours, weeks, or years
+// Converts into minutes, days, hours, weeks, years, decades, centuries
 // Reference: stackoverflow @ https://stackoverflow.com/questions/11792726/turn-seconds-into-hms-format-using-jquery
+// Takes in number representing # of seconds
+// Converts into minutes, days, hours, weeks, and years
+// Returns these values in an object
 
 const unitsOfTime = function (seconds) {
+
   let secondsLeft = seconds;
-
-  const centuries = Math.floor(secondsLeft / (604800 * 52 * 1000))
-  secondsLeft = secondsLeft - (centuries * 604800 * 52 * 1000)
-
-  const decades = Math.floor(secondsLeft / (604800 * 52 * 10))
-  secondsLeft = secondsLeft - (decades * 604800 * 52 * 10)
-
+  // how many centuries does this # of seconds contain?
+  const centuries = Math.floor(secondsLeft / (604800 * 52000))
+  secondsLeft = secondsLeft - (centuries * 604800 * 52000)
+  // with what's left, how many decades?
+  const decades = Math.floor(secondsLeft / (604800 * 520))
+  secondsLeft = secondsLeft - (decades * 604800 * 520)
+  // then how many years
   const years = Math.floor(secondsLeft / (604800 * 52));
   secondsLeft = secondsLeft - (years * 604800 * 52);
-
+  // and months
   const months = Math.floor(secondsLeft / (68400 * 30));
   secondsLeft = secondsLeft - (months * 68400 * 30);
-
+  // days
   const days = Math.floor(secondsLeft / 68400);
   secondsLeft = secondsLeft - (days * 68400);
-
+  // hours
   const hours = Math.floor(secondsLeft / 3600);
   secondsLeft = secondsLeft - (hours * 3600);
-
+  // minutes
   const minutes = Math.floor(secondsLeft / 60);
+  // and finally, this is how many seconds are left over
   secondsLeft = secondsLeft - (minutes * 60)
-
+  // make an object for timeBreakdown to use
   const timeBreakdown = {
+    centuries,
+    decades,
     years,
     months,
     days,
@@ -34,24 +41,34 @@ const unitsOfTime = function (seconds) {
     minutes,
     seconds: secondsLeft
   }
-
   return timeBreakdown
 }
 
 // Takes in an object of time with years, months, days, hours, minutes, and seconds
-// as keys with corresponding values
-// Returns a string indicating the number of the largest unit of time
+// as keys with corresponding values.
+// When it reaches the first key with a non-zero value,
+// returns a string indicating the number of the largest unit of time.
 
 const timeBreakdown = function (time) {
 
   for (const unit of Object.keys(time)) {
-    let convertToNumber = Number(time[unit]);
-    if (convertToNumber !== 0) {
-      return `${convertToNumber} ${unit}`
+    let count = Number(time[unit]);
+    if (count !== 0) {
+      return `${count} ${unit}`
     }
   }
 }
 
+// Takes in tweet object
+// Returns amount of time elapsed since date created to current time
 
-let test = unitsOfTime(222);
-console.log(timeBreakdown(test));
+const howLongAgoWasThisTweetCreated = function (tweet) {
+  
+  const currentTime = new Date().getTime();
+  const tweetCreatedAt = tweet.created_at;
+  const timeSinceTweet = currentTime - tweetCreatedAt;
+
+  const howLong = timeBreakdown(unitsOfTime(timeSinceTweet));
+
+  return `${howLong} ago`
+}
