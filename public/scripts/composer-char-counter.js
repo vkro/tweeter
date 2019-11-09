a0ffa9c89626481f33831c41e2738afa0ff82fc8
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-09 00:43:31 +0000

    cleanup and lint character counter script

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,19 @@
+// For each key input to text input area,
+// Take length of input and update display of characters left
+// If count < 0 add negative class to signal CSS change to red text
+
 $(document).ready(function() {
 
   $("#textArea").keyup(function() {
+
     let counter = $(this).parent().children("span");
     let count = ($(this).context.value).length;
     let charsLeft = 140 - count;
+
     if (charsLeft < 0) {
       counter.addClass("negative");
     } else {
-      counter.removeClass("negative")
+      counter.removeClass("negative");
     }
     $(counter).text(charsLeft);
   });
